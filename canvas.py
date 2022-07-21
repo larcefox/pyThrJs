a6cc91bb33011e833f03d566725ecbb73f0284bf
Larce Fox <larcethefox@gmail.com>
2022-07-21 23:12:40 +0300

    added position, rotation, geometry, need to add bootstrap, classes, texture

--- canvas.py
+++ canvas.py
@@ -5,32 +5,116 @@ import json
 def send_data():
     geometry = {
             'Object1':{
-                'type': 'BoxGeometry',
-                'id': 0,
-                'width': 1,
-                'height': 1,
-                'depth': 5,
-                'color': 0xff00ff,
+                'material_type': 'MeshBasicMaterial',
+                'geometry_type': 'BoxGeometry',
+                'geometry':{
+                    'width': 1,
+                    'height': 5,
+                    'depth': 1,
                 },
+                'material': {
+                    'color': 0xff00ff,
+                },
+                'position':{
+                    'x': 0,
+                    'y': 0,
+                    'z': 0,
+                    },
+                'rotation':{
+                    'x': 0,
+                    'y': 0,
+                    'z': 0,
+                    },
+            },
 
             'Object2':{
-                'type': 'BoxGeometry',
-                'id': 0,
-                'width': 5,
-                'height': 1,
-                'depth': 1,
-                'color': 0x00ff00,
-                    
+                'material_type': 'MeshBasicMaterial',
+                'geometry_type': 'BoxGeometry',
+                'geometry':{
+                    'width': 1,
+                    'height': 1,
+                    'depth': 5,
+                },
+                'material': {
+                    'color': 0x00ff00,
+                },
+                'position':{
+                    'x': 0,
+                    'y': 0,
+                    'z': 0,
+                    },
+                'rotation':{
+                    'x': 0,
+                    'y': 0,
+                    'z': 0,
+                    },
             },
             'Object3':{
-                'type': 'BoxGeometry',
-                'id': 0,
-                'width': 1,
-                'height': 5,
-                'depth': 1,
-                'color': 0x0000ff,
-                    
-            }
+                'material_type': 'MeshBasicMaterial',
+                'geometry_type': 'BoxGeometry',
+                'geometry':{
+                    'width': 5,
+                    'height': 1,
+                    'depth': 1,
+                },
+                'material': {
+                    'color': 0xff0000,
+                },
+                'position':{
+                    'x': 0,
+                    'y': 0,
+                    'z': 0,
+                    },
+                'rotation':{
+                    'x': 0,
+                    'y': 0,
+                    'z': 0,
+                    },
+            },
+            'Object4':{
+                'material_type': 'MeshBasicMaterial',
+                'geometry_type': 'SphereGeometry',
+                'geometry':{
+                    'radius': 1,
+                    'widthSegments': 32,
+                    'heightSegments': 16,
+                },
+                'material': {
+                    'color': 0x00ffff,
+                },
+                'position':{
+                    'x': 0,
+                    'y': 0,
+                    'z': 0,
+                    },
+                'rotation':{
+                    'x': 0,
+                    'y': 0,
+                    'z': 0,
+                    },
+            },
+
+            'Object5':{
+                'material_type': 'MeshBasicMaterial',
+                'geometry_type': 'PlaneGeometry',
+                'geometry':{
+                    'width': 10,
+                    'height': 10,
+                },
+                'material': {
+                    'color': 0xffffff,
+                },
+                'position':{
+                    'x': 0,
+                    'y': -5,
+                    'z': 0,
+                    },
+                'rotation':{
+                    'x': -120,
+                    'y': 0,
+                    'z': 0,
+                    },
+            },
     }
     return json.dumps(geometry)
 
--- templates/include/visualisation.js
+++ templates/include/visualisation.js
@@ -1,10 +1,8 @@
-//import './js/bootstrap.js';
-//import './css/bootstrap.min.css';
 import * as dat from './js/dat.js';
 import * as THREE from './js/three.js';
 import {OrbitControls} from './js/OrbitControls.js';
 
-
+// create button reload
 let btn = document.createElement("button");
 btn.innerHTML = "Reload";
 btn.ClassName = "btn btn-outline-primary";
@@ -14,8 +12,7 @@ btn.addEventListener("click", animate);
 document.body.appendChild(btn);
 
 const scene = new THREE.Scene();
-const camera = new THREE.
-    PerspectiveCamera(
+const camera = new THREE.PerspectiveCamera(
         75,
         innerWidth / innerHeight,
         0.1,
@@ -27,12 +24,10 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setSize(innerWidth, innerHeight)
 renderer.setPixelRatio(devicePixelRatio)
 document.body.appendChild(renderer.domElement)
-
 new OrbitControls(camera, renderer.domElement)
-let geometry = []
 
+// function for downloading json
 async function LoadJsonData(){
-     
     let url = 'http://10.8.0.2:9000/data';
     let response = await fetch(url);
     if (response.ok) { // if HTTP-status is 200-299
@@ -40,14 +35,18 @@ async function LoadJsonData(){
         console.log(data)
         // loop parse data grom canvas.py 
         for (const [key, value] of Object.entries(data)) {
-            if (value.type == 'BoxGeometry') {
-                const mesh = new THREE.Mesh(
-                    new THREE.BoxGeometry(value.width, value.height, value.depth),
-                    new THREE.MeshBasicMaterial({color: value.color}))
-                console.log(value.width)
-                console.log(value.id)
-                scene.add(mesh)
-            }
+            const figure = new THREE[value.geometry_type](...Object.values(value.geometry));
+            const material = new THREE[value.material_type](value.material);
+            const mesh = new THREE.Mesh(figure, material); 
+            mesh.name = key;
+            scene.add(mesh);
+
+            // add mesh positio
+            mesh.position.set(...Object.values(value.position));
+
+            // add mesh rotation
+            mesh.rotation.set(...Object.values(value.rotation));
+
         }
     }else {
         alert("HTTP-Error: " + response.status);
@@ -68,17 +67,17 @@ light2.position.set(0, 0, -1)
 
 scene.add(light1, light2)
 camera.position.z = 20
-
+camera.position.y = 20
+camera.rotation.y = 20
 
 renderer.render(scene, camera)
 
 function animate() {
-    requestAnimationFrame(animate)
-    renderer.render(scene, camera)
-    // mesh.rotation.x += 0.01
-    // mesh.rotation.y += 0.01
-    // planeMesh.rotation.z -= 0.01
-    // planeMesh.rotation.x -= 0.01
+     requestAnimationFrame(animate);
+     renderer.render(scene, camera);
+     //scene.getObjectByName( "Object1", true  ).rotation.x += 0.01;
+     //scene.getobjectbyname( "object2", true  ).rotation.y += 0.01;
+     //scene.getObjectByName( "Object3", true  ).rotation.z += 0.01;
 }
 
 animate()
