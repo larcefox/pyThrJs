274c79224b8c94d84a56ffed086770d948384634
Larce Fox <larcethefox@gmail.com>
2022-08-14 23:57:27 +0300

    new cranch for developing iboxrter

---
+++ modeller.py
@@ -0,0 +1,17 @@
+#!/home/larce/projects/pythr/env/bin/python
+# -*- coding: utf-8 -*-
+"""pythr.py: Visual modeling helper."""
+
+__author__ = "Bac9l Xyer"
+__copyright__ = "GPLv3"
+
+import socketserver
+from lib.server_start import HTTP_SERVER
+
+def run(hendler_class=HTTP_SERVER, port=9000):
+    server_address = ('0.0.0.0', port)
+    httpd = socketserver.TCPServer(server_address, hendler_class)
+    print('server started')
+    httpd.serve_forever()
+
+run()
